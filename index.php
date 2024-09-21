0c272ed32ff5ae72fcc4d566eeb0e83b54bd91ea
PaulThomasCodes <89669377+PaulThomasCodes@users.noreply.github.com>
2024-09-21 15:12:47 -0700

    My update hw for this week

--- index.php
+++ index.php
@@ -5,9 +5,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="main.css">
-    <script src 
+    <script src="script.js"></script>
 </head>
 <body>
-    
+    <!-- Here is the header; TODO: ADD AN IMAGE -->
+    <header>
+        <h1>Welcome to my Mars tourism Web Application</h1>
+    </header>
+
+    <!-- Here's the main seection -->
+    <main>
+       <p>
+            My company is awesome. we provide awesome trips to mars.
+        </p>    
+    </main>
+
+    <footer>
+
+    </footer>
 </body>
 </html>
