8c940855ac02413cff8d693c91451654a3e81b83
PaulThomasCodes <89669377+PaulThomasCodes@users.noreply.github.com>
2024-10-04 14:33:01 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Landee.com</title>
+    <link rel="stylesheet" href="main.css">
+    <script> src="script.js"></script>
+    
+</head>
+<body>
+    
+    <!-- here is the header; TODO: add AI image -->
+    <header>
+        <h1>Varified deeds you can trust</h1>
+    </header>
+    <!-- here's the main section -->
+    <main>
+   
+        <p>
+        Landee is an app that allows users to quickly verify that a land deed is authentic before they buy the property. 
+        </p>
+        <img src="land_picture.jpg" alt="land_picture" width="1500" height="600" >
+    </main>
+
+    <footer>
+
+    </footer>
+    <!-- I usede AI to create the logo -->
+</body>
+</html>
